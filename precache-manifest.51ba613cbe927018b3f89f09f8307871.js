de0b584ed9b2cb11e8172867b8bbb1a09eb27db4
Rahul Verma <rahul.verma@kbgassociates.biz>
2023-04-07 18:35:49 +0530

    Updates

---
+++ precache-manifest.51ba613cbe927018b3f89f09f8307871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0c614a718ff9323947",
+    "url": "/MyReactHome/static/js/main.1d0c614a.chunk.js"
+  },
+  {
+    "revision": "70cfe8e588d7e9996d27",
+    "url": "/MyReactHome/static/css/1.0d859108.chunk.css"
+  },
+  {
+    "revision": "70cfe8e588d7e9996d27",
+    "url": "/MyReactHome/static/js/1.70cfe8e5.chunk.js"
+  },
+  {
+    "revision": "64af3cd71a692d20d1bf",
+    "url": "/MyReactHome/static/js/runtime~main.64af3cd7.js"
+  },
+  {
+    "revision": "8cf937b7f4341726244096b0c5064ff4",
+    "url": "/MyReactHome/index.html"
+  }
+];
