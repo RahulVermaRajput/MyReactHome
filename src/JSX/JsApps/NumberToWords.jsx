f40f1887795402ddf6b2269bdc2c822bb50f9dfc
Rahul Verma <rahul.verma@kbgassociates.biz>
2023-11-27 11:58:15 +0530

    Done Amount to Word INR

--- src/JSX/JsApps/NumberToWords.jsx
+++ src/JSX/JsApps/NumberToWords.jsx
@@ -47,14 +47,74 @@ const NumberToWords = () => {
   }
 
   const handleNumeric = (value) => {
-    console.log(value);
+    //console.log(value);
     if (/^\d*$/.test(value)) {
         setAmount(value);
     }
   };
   
+  const convertIntoWordINR = (amount)=>{
+    const scales = ['','','','', 'thousand','thousand', 'lakh','lakh', 'crore','crore', 'arab','arab', 'kharab','kharab', 'neel','neel', 'padma','padma'];
+    let valueInt = Number(amount);
+    const value = amount+'';
+    let result = '';
+    let part = value.split('');
+    if(part.length%2===0 && part.length>3)
+    {
+      part = ['0',...part];
+    }
+    if(part.length>2)
+    {
+      valueInt =  Number(part[part.length-3]+part[part.length-2]+part[part.length-1])
+    } 
+    var pi = 0;
+    var li = part.length;
+    var si = part.length;
+    while(li>3)
+    {
+      let number = Number(part[pi++]+part[pi++]);
+      if(number>0)
+      {
+        result += addUpto99(number)+' '+scales[si]+' ';
+      }
+      si = si-2;
+      li = li-2;
+    }
+    result += addUpto999(valueInt);
+    //console.log(result);
+    return result;
+  }
+  
+  const addUpto999 = (valueInt)=>{  
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']; 
+    const pos = Math.floor(valueInt/100);
+    return  pos !== 0? units[pos] +" hundred " + addUpto99(valueInt%100): addUpto99(valueInt%100);
+  }
+
+  const addUpto99 = (valueInt)=>{
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let result = '';
+    if(valueInt > 0 && valueInt <10)
+    {
+        result = units[valueInt];
+    }
+    else if(valueInt >= 10 && valueInt <20)
+    {
+        result = teens[valueInt%10];
+    }
+    else if(valueInt >= 20 && valueInt <100)
+    {
+        result = tens[Math.floor(valueInt/10)] +" "+ units[valueInt%10];
+    }
+    return result;
+}
+
 
   const amountInWords = convert(amount);
+
+  const amountInWordsINR = capitalizeSentence(convertIntoWordINR(amount));
 
   return (
     <div>
@@ -67,166 +127,16 @@ const NumberToWords = () => {
             {/* <input type = "button" value = "Convert" className="btn btn-primary btn-xs me-3 mt-2" id = "convertBtn" onClick={convert}/> */}
         </div>
         <br/><br/>
-      <h4>Amount in Words: <i>{amountInWords}</i></h4>
+      <h4>Amount in Words:</h4> <h5><i>{amountInWords}</i></h5>
+      <br/><br/>
+      <h4>Amount in Words(INR):</h4> <h5><i>{amountInWordsINR}</i></h5>
+     
 
 
-    <AmountToIndianEnglishWords/>
-
+    
     </div>
   );
 };
 
-
-const AmountToIndianEnglishWords = () => {
-    const [inputAmount, setInputAmount] = useState('');
-    const [resultInWords, setResultInWords] = useState('');
-  
-    const convertToIndianEnglishWords = (amount) => {
-      const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-      const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-      const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-      const scales = ['hundred', 'thousand', 'lakh', 'crore', 'arab', 'kharab', 'neel', 'padma'];
-  
-      const splitIntoParts = (num) => {
-        const parts = [];
-        while (num > 0) {
-          parts.push(num % 100);
-          num = Math.floor(num / 100);
-        }
-        return parts;
-      };
-  
-      const toIndianEnglishWords = (num) => {
-        const part = [];
-        const hundreds = Math.floor(num / 100);
-        const tensUnits = num % 100;
-  
-        if (hundreds !== 0) {
-          part.push(units[hundreds] + ' hundred');
-        }
-  
-        if (tensUnits !== 0) {
-          if (tensUnits < 10) {
-            part.push(units[tensUnits]);
-          } else if (tensUnits < 20) {
-            part.push(teens[tensUnits - 10]);
-          } else {
-            part.push(tens[Math.floor(tensUnits / 10)] + (tensUnits % 10 !== 0 ? '-' + units[tensUnits % 10] : ''));
-          }
-        }
-  
-        return part.join(' ');
-      };
-  
-      if (amount === 0) {
-        return 'zero';
-      }
-  
-      const parts = splitIntoParts(amount);
-      let result = '';
-      for (let i = 0; i < parts.length; i++) {
-        if (parts[i] !== 0) {
-          result = toIndianEnglishWords(parts[i]) + ' ' + scales[i] + ' ' + result;
-        }
-      }
-  
-      return result.trim();
-    };
-  
-    const handleInputChange = (event) => {
-      const value = event.target.value;
-      if (!isNaN(value) && value !== '') {
-        setInputAmount(value);
-        setResultInWords(convertToIndianEnglishWords(Number(value)));
-      } else {
-        setInputAmount('');
-        setResultInWords('');
-      }
-    };
-  
-    const convertIntoWord = (event)=>{
-
-        const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        const scales = ['hundred', 'thousand', 'lakh', 'crore', 'arab', 'kharab', 'neel', 'padma'];
-        let result = '';
-        setInputAmount(event.target.value);
-        const value = event.target.value;
-        const valueInt = Number(event.target.value); 
-        const parts = value.split('');
-        //console.log(valueInt);
-        if(valueInt > 0 && valueInt <10)
-        {
-            result = units[valueInt];
-        }
-        else if(valueInt >= 10 && valueInt <20)
-        {
-            result = teens[valueInt%10];
-        }
-        else if(valueInt >= 20 && valueInt <100)
-        {
-            const position = Math.floor(valueInt/10); 
-            result = tens[position] +" "+ units[valueInt%10];
-        }
-        else if(valueInt >= 100 && valueInt <1000)
-        {
-            const position = Math.floor(valueInt/100); 
-            result = units[position] +" hundred "+ addUpto99(valueInt%100);
-        }
-        else if(valueInt >= 1000 && valueInt <100000)
-        {
-            const position = Math.floor(valueInt/100); 
-            result = units[position] +" hundred "+ addUpto99(valueInt%100);
-        }
-
-        console.log(result);
-
-    }
-
-    const addUpto99 = (valueInt)=>{
-
-        const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-        const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-        const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        const scales = ['hundred', 'thousand', 'lakh', 'crore', 'arab', 'kharab', 'neel', 'padma'];
-        let result = '';
-        if(valueInt > 0 && valueInt <10)
-        {
-            result = units[valueInt];
-        }
-        else if(valueInt >= 10 && valueInt <20)
-        {
-            result = teens[valueInt%10];
-        }
-        else if(valueInt >= 20 && valueInt <100)
-        {
-            const position = Math.floor(valueInt/10); 
-            result = tens[position] +" "+ units[valueInt%10];
-        }
-        return result;
-    }
-
-
-
-    return (
-      <div>
-        <h3>Convert Amount to Indian English Words (Upto 15 digits)</h3>
-        <input
-          type="text"
-          value={inputAmount}
-        //   onChange={handleInputChange}
-          onChange={convertIntoWord}
-          placeholder="Enter an amount"
-        />
-
-     
-
-        <div>
-          <strong>Amount in Indian English Words:</strong> {resultInWords}
-        </div>
-      </div>
-    );
-  };
 
 export default NumberToWords;
