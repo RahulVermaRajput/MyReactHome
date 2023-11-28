eda6781529da85788ae91280d3a1a2e9ce8026c8
Office PC <rahul.verma@kbgassociates.biz>
2023-11-28 17:46:23 +0530

    clock done with copy of codepen

--- src/JSX/JsApps/Clock.jsx
+++ src/JSX/JsApps/Clock.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import '../../CSS/clock.css'; // Import clock styles
+import Test from '../Test';
 
 const Clock = () => {
     const [time, setTime] = useState(new Date());
@@ -33,30 +34,45 @@ const Clock = () => {
     };
   
     const { secondHand, minuteHand, hourHand } = getTransformStyles();
-  
+    const dialArray = [];
+
+    for (let i = 1; i <= 60; i++) {
+      dialArray.push(<span className='dialLines' key={i} id={i} style={{transform:`rotate(${6*i}deg)`}}></span>);
+    }
+
     return (
+      <>
+      <h2>asdfasd</h2>
       <div className="clock">
-        <div className="dial">
-          <div className="number number-12">12</div>
-          <div className="number number-1">1</div>
-          <div className="number number-2">2</div>
-          <div className="number number-3">3</div>
-          <div className="number number-4">4</div>
-          <div className="number number-5">5</div>
-          <div className="number number-6">6</div>
-          <div className="number number-7">7</div>
-          <div className="number number-8">8</div>
-          <div className="number number-9">9</div>
-          <div className="number number-10">10</div>
-          <div className="number number-11">11</div>
-          
+        <div className="dial"></div>
+          {
+            dialArray
+          }
           <div className="hand hour-hand" style={{ transform: hourHand }} />
           <div className="hand minute-hand" style={{ transform: minuteHand }} />
           <div className="hand second-hand" style={{ transform: secondHand }} />
           <div className="center-circle" />
-        </div>
+      
+        <div className="number number-12">12</div>
+          <div className="number number-3">3</div>
+          <div className="number number-6">6</div>
+          <div className="number number-9">9</div>
+          {/* <div className="number number-1">1</div>
+          <div className="number number-2">2</div>
+          <div className="number number-4">4</div>
+          <div className="number number-5">5</div>
+          <div className="number number-7">7</div>
+          <div className="number number-8">8</div>
+          <div className="number number-10">10</div>
+          <div className="number number-11">11</div> */}
+
       </div>
+      <hr/><hr/>
+      <Test/>
+      </>
     );
   };
   
+
+
 export default Clock;
